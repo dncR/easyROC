9664b668dedf464e03a9bd87ba5302f446df09db
Dinçer GÖKSÜLÜK (Windows) <dincer.goksuluk@gmail.com>
2021-09-03 16:34:16 +0300

    Rearrangement of folders and files.

--- installDependencies.R
+++
@@ -1,6 +0,0 @@
-## Add required packages for the shiny app here.
-install.packages("dplyr")
-install.packages("magrittr")
-install.packages("pROC")
-install.packages("OptimalCutpoints")
-install.packages("plyr")
---
+++ installeR/install.R
@@ -0,0 +1,2 @@
+## Add required packages for the shiny app here.
+install.packages(c("dplyr", "OptimalCutpoints", "magrittr", "pROC", "plyr"))
